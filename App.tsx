5e85c1dd86becceab513dc441dee9d3df6776f3c
Nguyễn Vĩnh Thanh Chương <nvtc.98@gmail.com>
2021-02-23 16:05:36 +0700

    init eslint + react navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+      </View>
+    </NavigationContainer>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
